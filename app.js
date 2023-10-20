750e0479443e4826f9d371855355b3ef8f8d4e87
Conor <conor.reed@gmail.com>
2023-10-20 13:56:10 -0500

    created a variable that holds the filepath of the db.json file and passed it into the API routes

--- app.js
+++ app.js
@@ -12,34 +12,36 @@ app.use(express.static('public'));
 
 // HTML Routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'db', 'public', 'notes.html'));
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'db', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
+const dbFilePath = path.join(__dirname, 'db', 'db.json');
+
 // API Routes
 app.get('/api/notes', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('db.json'));
+  const data = JSON.parse(fs.readFileSync(dbFilePath));
   res.json(data);
 });
 
 app.post('/api/notes', (req, res) => {
   const newNote = req.body;
   newNote.id = uuidv4(); 
-  const data = JSON.parse(fs.readFileSync('db.json'));
+  const data = JSON.parse(fs.readFileSync(dbFilePath));
   data.push(newNote);
-  fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
+  fs.writeFileSync(dbFilePath, JSON.stringify(data, null, 2));
   res.json(newNote);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
   const idToDelete = req.params.id;
-  const data = JSON.parse(fs.readFileSync('db.json'));
+  const data = JSON.parse(fs.readFileSync(dbFilePath));
   const updatedData = data.filter((note) => note.id !== idToDelete);
-  fs.writeFileSync('db.json', JSON.stringify(updatedData, null, 2));
+  fs.writeFileSync(dbFilePath, JSON.stringify(updatedData, null, 2));
   res.json({ message: 'Note deleted' });
 });
 
